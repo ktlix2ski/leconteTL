965ab89fa3ee8bb9413db8886f43026dba010d53
ktlix2ski <troyekay@oregonstate.edu>
2024-04-05 12:25:40 -0700

    new stuff to get folder open

--- timelapse.py
+++ timelapse.py
@@ -11,18 +11,21 @@ import numpy as np
 from PIL import Image
 from PIL.ExifTags import TAGS
 import matplotlib.pyplot as plt
+import os
 
 # testing for individual images
 
 image_path = "/home/kayatroyer/Desktop/Leconte/Test_Imgs/091823/IMG_1491.JPG"
 image = Image.open(image_path)
 exif_data = image.getexif()
+file_size = os.path.getsize(image_path)
 
 # getting image data
 print("Image Format:", image.format) 
 print("Image Mode:", image.mode) 
 print("Image Size:", image.size) 
 print("Image Info:", exif_data.get(306))
+print("File Size",file_size/(1e12), "TB")
         
 
 #   graph of pixel data
@@ -30,3 +33,22 @@ print("Image Info:", exif_data.get(306))
 # plt.xlabel('bands')
 # plt.ylabel('count')
 # plt.show()
+
+# taking in a folder of images
+folder_path = "/home/kayatroyer/Desktop/Leconte/091823"
+image_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path,f))]
+
+
+
+#folder_data = folder_path.getexif()
+
+# image_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path,f))]
+# for file_name in image_files:
+#     try:
+#         image = Image.open(os.path.join(folder_path, file_name))
+#         image.show()
+#     except IOError:
+#         print(f"unable to open {file_name}")
+
+#folder = Image.open(folder_path)
+# folder_data = folder.getexif()
